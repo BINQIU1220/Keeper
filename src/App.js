697801406892b71513075886de52e70a76c090af
binqiu1220 <ezio12a@hotmail.com>
2024-04-05 11:06:37 +0100

    initialise Header, Note and Footer component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Copyright @ {new Date().getFullYear()}</p>
-      </header>
+      <Header />
+      <Note />
+      <Footer />
     </div>
   );
 }

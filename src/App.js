9a6ca4bda2ba0ca22f26391f80bfa26da28cae11
binqiu1220 <ezio12a@hotmail.com>
2024-04-05 11:00:54 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +12,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>Copyright @ {new Date().getFullYear()}</p>
       </header>
     </div>
   );
